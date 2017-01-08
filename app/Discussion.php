889cecf545cc601be7eb9628af4b2450cf431816
高伟 <gaowei@gaoweideMBP.lan>
2017-01-08 14:45:58 +0800

    生产测试数据

--- app/Discussion.php
+++ app/Discussion.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Discussion extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title','body','user_id','last_user_id'];
 }
