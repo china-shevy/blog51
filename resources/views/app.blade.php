82c3ab5f152ef4a04d63a0c45f534d7fc8f4b708
高伟 <gaowei@gaoweideMBP.lan>
2017-02-19 16:20:09 +0800

    顶部菜单 完善

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Blog</title>
-	<link href="/css/bootstrap.css" rel="stylesheet">
+  <link href="/css/bootstrap.css" rel="stylesheet">
+	<link href="/css/font-awesome.min.css" rel="stylesheet">
 	<link href="/css/style_v1.css" rel="stylesheet">
 </head>
 <body>
@@ -25,9 +26,17 @@
           </ul>
           <ul class="nav navbar-nav navbar-right">
           @if (Auth::check())
-            <li><a href="#">{{ Auth::user()->name }}</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuDivider">
+                <li><a href=""><i class="fa fa-user"></i> 更换头像</a></li>
+                <li><a href=""><i class="fa fa-cog"></i> 更换密码</a></li>
+                <li><a href=""><i class="fa fa-heart"></i> 特别感谢</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="/user/logout"><i class="fa fa-sign-out"></i> 退出登录</a></li>
+              </ul>
+            </li>
             <li><a href="#" class="nav-avatar-info"><img src="{{ Auth::user()->avatar }}" class="img-circle" width="50"></a></li>
-            <li><a href="/user/logout">退出登录</a></li>
           @else
             <li><a href="/user/login">登录</a></li>
             <li><a href="/user/register">注册</a></li>
@@ -38,4 +47,6 @@
     </nav>
 	@yield('content')
 </body>
+<script src="/js/jquery-3.1.1.js"></script>
+<script src="/js/bootstrap.min.js"></script>
 </html>
